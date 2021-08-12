b93e11441ed942f1fb37533f1e9b4465dbca9d2c
Baktiiar <bkukanov@gmail.com>
2021-08-12 22:57:52 +0600

    NFC chip pins corrected

--- Inc/main.h
+++ Inc/main.h
@@ -60,20 +60,20 @@ void SystemClock_CustomConfig(bool nfc_low_power, bool pll_reconfig);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define FM_IRQN_Pin GPIO_PIN_0
-#define FM_IRQN_GPIO_Port GPIOA
+#define FM_IRQN_Pin GPIO_PIN_15
+#define FM_IRQN_GPIO_Port GPIOC
 #define FM_IRQN_EXTI_IRQn EXTI0_IRQn
-#define FM_SSN_Pin GPIO_PIN_4
-#define FM_SSN_GPIO_Port GPIOA
+#define FM_SSN_Pin GPIO_PIN_0
+#define FM_SSN_GPIO_Port GPIOB
 #define LED_Pin GPIO_PIN_3
-#define LED_GPIO_Port GPIOB
+#define LED_GPIO_Port GPIOA
 #define TOUCH_Pin GPIO_PIN_3
 #define TOUCH_GPIO_Port GPIOH
 /* USER CODE BEGIN Private defines */
 #define HW_CFG_Port GPIOB
 #define HW_CFG0_Pin GPIO_PIN_4
 #define HW_CFG1_Pin GPIO_PIN_5
-#define HW_CFG2_Pin GPIO_PIN_6
+#define HW_CFG2_Pin GPIO_PIN_3
 #define HW_CFG3_Pin GPIO_PIN_7
 /* USER CODE END Private defines */
 
